da7dd263b973635a1cc6bbe28dc2a86a6b515384
ArnedyNavi <alfonsus737@gmail.com>
2022-07-12 23:45:30 +0700

    Add currency and date formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,11 +3,15 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeExpense } from "../reducers/expenses";
 import moment from "moment";
+import numeral from "numeral"
 
 const ExpenseListItem = (props) => (
     <div>
         <h3><Link to={"/edit/" + props.expense.id}>{props.expense.description}</Link></h3>
-        <p>$ {props.expense.amount} - {moment(props.expense.createdAt).format("MMM DD YYYY")}</p>
+        <p>{numeral(props.expense.amount).format('$0,0,0.00')} 
+             -  
+            {moment(props.expense.createdAt).format("MMMM Do YYYY")}
+        </p>
     </div>
 )
 
